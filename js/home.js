1d3451df32a46e651c480dbb70d2bc24ee63e658
DanLowo <danlowo@gmail.com>
2021-11-09 06:44:51 +0100

    hover on work-card

--- js/home.js
+++ js/home.js
@@ -1,20 +1,11 @@
-const ok =`
-    <h3>High-end custom residential renovators serving Fraser valley homeowners</h3>
-    <button class="custom-button">Visit Website</button>
-    `
-const card = `diavvpj`
-
 $(document).ready(function() {
-    $(".work-card").html(card)
-    let watchClickedCard;
 
-    $(".work-card").click(function() {
-        // remove 'active' class from previous work-card and set html back to normal
-        $(watchClickedCard).removeClass("active")
-        $(watchClickedCard).html(card)
+    // handle mouseover and mouseout on work-card
+    $(".work-card").mouseover(function() {
+        $(this).addClass("active")
+    })
 
-        // store the current clicked card in the watch var than add class and html to it
-        watchClickedCard = this;
-        $(this).addClass("active").html(ok)
+    $(".work-card").mouseout(function() {
+        $(this).removeClass("active")
     })
 })
