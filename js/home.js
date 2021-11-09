319971f02eb2dbbd2426e15be92867bcaba2699e
DanLowo <danlowo@gmail.com>
2021-11-09 07:56:03 +0100

    mobile navigation with animation

--- js/home.js
+++ js/home.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
+    // mobile navigation
+    $(".mobile-navigation").hide()
+
+    // open mobile navigation
+    $("nav .open-nav-icon").click(function() {
+        $(".mobile-navigation div.contents").hide()
+        $(".mobile-navigation").show()
+
+        setTimeout(function() {
+            $(".mobile-navigation div.contents").show()
+        }, 1000)
+    })
+
+    // close mobile navigation
+    $(".mobile-navigation .close-nav-icon").click(function() {
+        $(".mobile-navigation").hide()
+    })
+
     // handle mouseover and mouseout on work-card
     $(".work-card").mouseover(function() {
         $(this).addClass("active")
